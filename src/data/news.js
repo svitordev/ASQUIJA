2aaa53f547c9b8851aaa4c8499faed2a6572f684
svitordev <vtexsilvas2@gmail.com>
2025-06-05 16:46:40 -0300

    adicionado nova noticia

--- src/data/news.js
+++ src/data/news.js
@@ -1,5 +1,10 @@
 export const news = [
   {
+    title: "🎬📢 O Cine CAIXA CAP está chegando em João Alfredo-PE com sessões gratuitas de cinema!",
+    srcImg: "/news/cineCaixaCap.jpg",
+    link: "https://www.instagram.com/p/DKh_O_SvsKs/",
+  },
+  {
     title: "📚📣✨ 26 de maio, damos início ao Projeto Quilombo Forte!",
     srcImg: "/news/quilomboForte2.jpg",
     link: "https://www.instagram.com/p/DKIjcfGgNs7/",
